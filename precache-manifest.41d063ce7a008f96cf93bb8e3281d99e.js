36717ac02086cc8c1e6645455c9543931d65d0c4
luciebaker <luciebdev@gmail.com>
2019-11-04 06:17:33 -0500

    Updates

---
+++ precache-manifest.41d063ce7a008f96cf93bb8e3281d99e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1da5c1bafd30eb6c66efe31029b29185",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "d45db5cf5151989566e7",
+    "url": "/monsters/static/css/main.706fa1bb.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "d45db5cf5151989566e7",
+    "url": "/monsters/static/js/main.d7917bf8.chunk.js"
+  },
+  {
+    "revision": "9487a71755d2d296bed6",
+    "url": "/monsters/static/js/runtime-main.dc640463.js"
+  }
+]);
